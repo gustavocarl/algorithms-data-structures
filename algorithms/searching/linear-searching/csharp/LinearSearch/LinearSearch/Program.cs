96198f3ff0b20b14f8c55e02b78451ce1ac68951
Gustavo Henrique Carl Dos Santos <gustavo_carl@hotmail.com>
2026-02-24 23:02:32 -0300

    Added the linear-searching in repository

---
+++ algorithms/searching/linear-searching/csharp/LinearSearch/LinearSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿var numbers = new int[10] {1, 2,3,4,5,6,7,8,9,10 };
+const int searchValue = 8;
+
+for (var i = 0; i < numbers.Length; i++){
+    if (searchValue == numbers[i])
+    {
+        Console.WriteLine($"O elemento {numbers[i]} foi encontrado!");
+        break;
+    }
+}
+
+Console.ReadLine();
